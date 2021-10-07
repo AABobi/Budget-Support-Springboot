71efab9a1692da65b72ed04f3371f74b7b14d660
Radosław Kopeć <radoslawkopec93@gmail.com>
2021-10-07 18:27:55 +0200

    First commit

---
+++ src/main/java/pl/radoslaw/kopec/BudgetSupportBackend/BudgetSupportBackEndApplication.java
@@ -0,0 +1,13 @@
+package pl.radoslaw.kopec.BudgetSupportBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BudgetSupportBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BudgetSupportBackEndApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/radoslaw/kopec/BudgetSupportBackend/BudgetSupportBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.radoslaw.kopec.BudgetSupportBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BudgetSupportBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
